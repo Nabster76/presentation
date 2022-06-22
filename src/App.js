818144346808b94b6578d38a0d21070279147be1
Nabil Laaziri <n.laaziri@gmail.com>
2022-06-22 03:31:49 +0200

    initil commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { Download, Features, SectionWrapper } from './components';
+import assets from './assets';
+import styles from './styles/Global';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <SectionWrapper 
+        title="Vous possédez un magasin de Nifty NFT. Commencez à vendre et à grandir"
+        description="Achetez, stockez, collectez des NFTS, échangez et gagnez des cryptos. Rejoignez plus de 25 millions de personnes utilisant Naclopop Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper 
+        title="Smart User Interface Marketplace"
+        description="Découvrez une interface utilisateur futuriste de Naclopop NFT Marketplace. Couleurs constantes lisses d'une conception d'interface utilisateur fluide."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+      <SectionWrapper 
+        title="Déploiement"
+        description="Naclopop est construit à l'aide d'Expo qui s'exécute nativement sur les appareils de tous les utilisateurs. Vous pouvez facilement mettre votre application entre les mains des gens"
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper 
+        title="Façon créative de mettre en valeur le place de marché"
+        description="L'application contient deux écrans. Le premier écran répertorie tous les NFT tandis que le second affiche les détails d'un NFT spécifique."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {" "}
+        <span className="bold">Nabil Laaziri</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Download.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import styles from '../styles/Global';
+import assets from '../assets';
+
+const Download = () => {
+  return (
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>Télécharger le code source</h1>
+          <p className={`${styles.pText} ${styles.blackText}`}>Obtenez le code source complet sur GitHub</p>
+        </div>
+        <button className={styles.btnPrimary}>Code Source</button>
+        <div className={styles.flexCenter}>
+          <img 
+            src={assets.scene}
+            alt="download_png"
+            className={styles.fullImg}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Download
